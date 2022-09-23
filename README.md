15f6425d75179941c2e3fae4592f9fb0c20a100b
Céline Eap <eapceline@gmail.com>
2022-09-23 23:10:59 +0200

    :memo: ADD readme & commentaires

---
+++ README.md
@@ -0,0 +1,10 @@
+-------- Projet pétanque --------
+
+Notre projet lors de la semaine de Créativ'lab a été de monter une boule de pétanque
+détecteuse de distance afin de calculer la distance entre une boule de pétanque et le cochonnet
+pour faciliter les joueurs de pétanque dans leur jeu.
+
+Pour celà, nous avons relier un capteur ultrason à une carte arduino ainsi qu'à un afficheur 4 digit pour
+afficher la distance que le capteur va calculer. Nous avons aussi ajouter un bouton faisant office d"interrupteur" qui allumera ou éteindra l'afficheur.
+
+-Angel Hmeli & Céline Eap-
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <TM1637Display.h>
 #define CLK 12
 #define DIO 13
+
+
 const uint8_t SEG_DONE[] = {
 	SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,
 	SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,
@@ -36,17 +38,20 @@ const long interval = 1000;
 unsigned long previousMillis = 0;
 int mDistanceInCm = 0;
 
+// initialiser l'état du bouton
 int lastBT = 0;
-int ledSTATE = 0;
+int btSTATE = 0;
 
 
 void setup() {
   // initialise la communication série
   Serial.begin(9600);
 
+  // configuration l'entrée du bouton
   pinMode(3, INPUT);
   ledComponent.Setup();
   
+  // congiguration de l'afficheur 4 digit
   uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
   display.setBrightness(0x0f);
   data[0] = display.encodeDigit(0);
@@ -59,22 +64,25 @@ void setup() {
 }
 
 void loop() {
+
+  // identifier l'état du bouton
   int etatBT = digitalRead(3);
   if (lastBT == 0 && etatBT == 1) {
     lastBT = 1;
     delay(100);
-    ledSTATE = 1;
+    btSTATE = 1;
     delay(100);
   }
   else if (lastBT == 1 && etatBT == 1) {
     lastBT = 0;
     delay(100);
-    ledSTATE = 0;
+    btSTATE = 0;
     delay(100);
   }
 
+  // afficher sur l'afficheur la distance calculée
   if (mDistanceInCm <= 1000){
-    if (ledSTATE == 1) {
+    if (btSTATE == 1) {
       delay(100);
       ledComponent.Display(1, 3);
       delay(100);
@@ -85,6 +93,8 @@ void loop() {
     }
   }
   
+  // calculer la distance grâce au module ultrasonic.Ranging qui retourne en centimètre la distance
+  // la distance est caluculer en miliseconde avec la fonction millis()
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
